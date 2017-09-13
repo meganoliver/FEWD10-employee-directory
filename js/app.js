24dab10434942518d748113806c3d317d95e4fd4
pdxoliver <rms.moliver@gmail.com>
2017-09-12 21:19:10 -0700

    Added search function.

--- js/app.js
+++ js/app.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
 
     $.each(employees.results, function(i, person) {
       //create variables for all data
-      let photo = person.picture.medium;
+      let photo = person.picture.large;
       let firstName = person.name.first;
       let lastName = person.name.last;
-      let fullName = firstName + " " + lastName;//done capitalizing
+      let fullName = firstName + " " + lastName;
       let email = person.email;
       let phone = person.cell;
       let street = '<span class="capitalize">' + person.location.street + '</span>';
@@ -27,8 +27,8 @@ $(document).ready(function () {
       empHTML += '<section class="emp-card">';
       empHTML += '<img class="emp-photo" alt="employee picture" src=' + photo + '>';
       empHTML += '<ul class="emp-info">';
-      empHTML += '<li class="name capitalize">' + fullName + '</li>';
-      empHTML += '<li>' + email + '</li>';
+      empHTML += '<li class="empName capitalize">' + fullName + '</li>';
+      empHTML += '<li">' + email + '</li>';
       empHTML += '<li>' + city + '</li></ul></section>';
 
     //modal div
@@ -40,8 +40,8 @@ $(document).ready(function () {
       modHTML += '<span class="next">' + 'next >' + '</span></div>';
       modHTML += '<div class="mod-info">';
       modHTML += '<img class="mod-photo" alt="employee picture" src=' + photo + '>';
-      modHTML += '<li class="name capitalize">' + fullName + '</li>';
-      modHTML += '<li>' + email + '</li>';
+      modHTML += '<li class="modName capitalize">' + fullName + '</li>';
+      modHTML += '<a href="mailto:' + email + '">' + email + '</a>';
       modHTML += '<li>' + city + '</li></div>';
       modHTML += '<div class=".modDetails">';
       modHTML += '<li>' + phone + '</li>';
@@ -53,27 +53,29 @@ $(document).ready(function () {
       selectHTML += '<option class="options" value="' + fullName + '">';
 
     }); //end html each loop
-      empHTML += '</div>';
-      modHTML += '</div>';
-      selectHTML += '</div>';
-      $('#employees').html(empHTML);
-      $('#modal').html(modHTML);
-      $('#select').html(selectHTML);
+
+    empHTML += '</div>';
+    modHTML += '</div>';
+    selectHTML += '</div>';
+    $('#employees').html(empHTML);
+    $('#modal').html(modHTML);
+    $('#select').html(selectHTML);
 
       $('section').click(function() {
         $('#overlay').css("visibility", "visible");
+        $('#mod-container').css("z-index", "5");
         let empCard = document.getElementsByClassName('emp-card');
         let modCard = document.getElementsByClassName('mod-card');
         let index = $(this).index();
         $(modCard[index]).css("left", "28%");
         $(modCard[index]).css("transition", "left .5s ease-in");
-      });//end click function
+      });//end open modal click
 
       $('.previous').click(function() {
         let modCard = document.getElementsByClassName('mod-card');
         let index = $(this.parentElement.parentElement.parentElement).index();
         if(index > 0) {
-          $(modCard[index]).css("left", "100%");
+          $(modCard[index]).css("left", "150%");
           $(modCard[index - 1]).css("left", "28%");
           $(modCard[index]).css("transition", "none");
         }
@@ -82,8 +84,8 @@ $(document).ready(function () {
       $('.next').click(function() {
         let modCard = document.getElementsByClassName('mod-card');
         let index = $(this.parentElement.parentElement.parentElement).index();
-        if(index < 11) {
-          $(modCard[index]).css("left", "100%");
+        if(index < modCard.length - 1) {
+          $(modCard[index]).css("left", "150%");
           $(modCard[index + 1]).css("left", "28%");
           $(modCard[index]).css("transition", "none");
         }
@@ -91,21 +93,33 @@ $(document).ready(function () {
 
       $('span').click(function() {
         if(this.className === 'close') {
-          $('.mod-card').css("left", "100%");
+          $('#mod-container').css("z-index", "-5");
+          $('.mod-card').css("left", "150%");
+          $('.mod-card').css("transition", "left .5s ease-in")
           $('#overlay').css("visibility", "hidden");
+          ;
         }
       });//end close click
 
-      $('.options').click(function() {
-        let empCard = document.getElementsByClassName('emp-card');
-        let options = document.getElementsByClassName('options');
-        console.log(options);
-        let index = $(this.index());
-        if(this[index] != empCard[index]) {
-          empCard.hide;
-        }
-      });
+      $('#select').keyup(function() {
+        let search = $(this).val().toLowerCase(); //collect search
+        let names = [];
+        $('.modName').each(function() {//create array of names
+          names.push($(this).text())
+        }); //end each loop
+        $.each(names, function(i, name) {
+          if(search === name) {
+            $('#overlay').css("visibility", "visible");
+            $('#mod-container').css("z-index", "5");
+            let empCard = document.getElementsByClassName('emp-card');
+            let modCard = document.getElementsByClassName('mod-card');
+            let index = $(this).index();
+            $(modCard[i]).css("left", "28%");
+            $(modCard[i]).css("transition", "left .5s ease-in");
+          } //end if statement
+        });
 
+      }); //end keyup
 
   } //end displayInfo function
   $.getJSON(url, displayInfo);
